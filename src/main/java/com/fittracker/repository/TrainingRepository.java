86b149ed4eb49d88660cbb0f0c17a8cccaf0b75b
FlyTy <99266951+raulgelsleichter@users.noreply.github.com>
2025-12-10 15:50:40 -0300

    Deletar codigo antigo

--- src/main/java/com/fittracker/repository/TrainingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fittracker.repository;
-
-import com.fittracker.entity.Training;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TrainingRepository extends JpaRepository<Training, Long> {
-}
